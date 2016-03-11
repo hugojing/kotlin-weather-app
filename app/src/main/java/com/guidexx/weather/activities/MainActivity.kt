85fd6609c87674b60e54dfafe5571e7ebd9316da
Hugo Jing <hugojingcn@gmail.com>
2016-03-11 18:11:20 +0800

    Play RecyclerView.

---
+++ app/src/main/java/com/guidexx/weather/activities/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.guidexx.weather.activities
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import com.guidexx.weather.R
+import kotlinx.android.synthetic.main.activity_main.*
+import com.guidexx.weather.adapters.ForecastListAdapter
+import android.support.v7.widget.LinearLayoutManager
+
+class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+    }
+
+}
